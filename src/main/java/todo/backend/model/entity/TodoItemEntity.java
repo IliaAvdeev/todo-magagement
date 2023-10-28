0cafb1c182899463a667124dc4e990b47a772424
Ilia Avdeev <iavdeev@kotelov.team>
2023-10-29 00:07:40 +0200

    Add @CreationTimestamp annotation for 'TodoItemEntity' to save creation time

--- src/main/java/todo/backend/model/entity/TodoItemEntity.java
+++ src/main/java/todo/backend/model/entity/TodoItemEntity.java
@@ -4,18 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
 @Entity
-public class TodoItemEntity {
+public class TodoItemEntity implements HasId {
     @Id
     @GeneratedValue
     private UUID id;
     private TodoStatus status;
     private String description;
+    @CreationTimestamp
     private OffsetDateTime createdTime;
     private OffsetDateTime dueDatetime;
     private OffsetDateTime markedDoneTime;
